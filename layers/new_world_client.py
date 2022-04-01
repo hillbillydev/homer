03ca08ca15c0ebb71930ee778de1df916e69caa1
Marcus Karlsson <hillbillydev@gmail.com>
2022-04-02 08:41:07 +1300

    feat(layers): move new world client into a layer
    by moving the new world libary and some
    shared dependencies, it let me have way
    smaller lambda sizes.  We are talking about
    a difference between ~50mb -> 2kb.

---
+++ layers/new_world_client.py
@@ -0,0 +1,112 @@
+import cloudscraper
+import backoff
+import requests
+
+
+class NewWorldClient():
+
+    products_dict = {
+        "butter": {
+           "sale_type": "UNITS",
+           "quantity": 1,
+           "productid": "5002842-EA-000NW",
+           "emoji": "🧈"
+        },
+        "eggs": {
+           "sale_type": "UNITS",
+           "quantity": 1,
+           "productid": "5008394-EA-000",
+           "emoji": ""
+        },
+        "ice_cream": {
+           "sale_type": "UNITS",
+           "quantity": 1,
+           "productid": "5008394-EA-000",
+           "emoji": ""
+        }
+        # Bacon
+        # Milk
+        # Coffee
+        # Tea
+        # Potato
+        # Pumpkin
+        # Brocolli
+        # Colliflower
+        # Carrots
+        # Apples
+        # Oranges
+        # Bananas
+        # Cranberry Juice
+        # Pepsi Can
+        # Mince
+        # Chicken
+        # Vanilla Ice Cream
+        # Cream Egg Ice Cream
+        # Burger Buns
+        # Frozen Peas
+        # Cocholate Bars
+        # Dog Food
+        # Cat Food
+        # Toilet Paper
+        # Kitchen Towels
+        # Tissues
+        # Family Chips
+            # One Sticky Ribs
+            # One Vinegar
+            # One Doritus
+        # Baby Wipes
+        # Nappy Bags
+        # Onion
+        # Garlic
+        # Dog Treat
+    }
+
+    def __init__(self, username, password):
+        self.token = self.__login(username, password)
+
+    def get_readable_products(self):
+        return [x.replace("_", " ").title() for x in self.products_dict]
+
+    @backoff.on_exception(backoff.expo,
+                          requests.exceptions.HTTPError,
+                          max_time=8)
+    def add_products_to_basket(self, products):
+        session = cloudscraper.create_scraper()
+
+        products_to_add = [self.products_dict[product.lower()] for product in products]
+
+        payload = {
+            'products': products_to_add
+        }
+
+        cookies = {
+            'SessionCookieIdV2': self.token
+        }
+
+        res = session.post(
+            "https://www.newworld.co.nz/CommonApi/Cart/Index",
+            json=payload,
+            cookies=cookies,
+        )
+
+        res.raise_for_status()
+
+    @backoff.on_exception(backoff.expo,
+                          requests.exceptions.HTTPError,
+                          max_time=8)
+    def __login(self, username, password):
+        payload = {
+              "email": username,
+              "password": password
+        }
+
+        session = cloudscraper.create_scraper()
+
+        res = session.post(
+            "https://www.newworld.co.nz/CommonApi/Account/Login", data=payload)
+
+        res.raise_for_status()
+
+        return res.cookies.get("SessionCookieIdV2")
+
+
